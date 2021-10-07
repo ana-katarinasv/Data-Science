e00d96081740fb6b7c3362f43858482f5cdecc59
Ana <s1829945@ed.ac.uk>
2021-10-07 11:34:56 +0100

    Ana updated coding etiquette script

--- coding_club_tutorials/coding_etiquette.R
+++ coding_club_tutorials/coding_etiquette.R
@@ -93,7 +93,7 @@ ggplot(LPI_biome_summ, aes(biome, color = biome, y = populations)) + geom_bar(st
   theme(legend.position = "none")
 dev.off()
 
-#Following coding syntax etiquette ----
+# Following coding syntax etiquette ----
 
 #object names should be concise and meaningful
 #object,function, variable names should be lowercase
@@ -113,4 +113,117 @@ calculate.avg.clicks  # This is what we are aiming for.
 CalculateAvgClicks  # Not that bad, but mixing capital and lowercase letters can lead to typos
 calculate_avg_clicks 
 calculateAvgClicks  # Bad. The convention is that functions are defined using dots, not underscores.
+
+
+
+# Spacing ----
+# place spaces around all inflix operators (=,+,-,<-,etc.)
+# always put space after comma
+# : and :: don't need spaces
+
+x <- 1:10  # Good
+base::get  # Good
+dplyr::select  # When you use `package_name::function_name` in your code like the example here, this means you are calling the function `select()` from the package `dplyr`
+# this way of using functions works without having loaded the package beforehand using `library(dplyr)`, but it's not very commonly used, since it's longer.
+
+crs.geo <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")  # geographical, datum WGS84
+# Here we are creating an imaginary object with a geographical projection commonly used for the UK
+
+# no spaces before left parenthesis except in a function call
+# Good
+if (debug) do(x)
+plot(x, y)
+# Bad
+if(debug)do(x)
+plot (x, y)
+
+# extra spacing is ok if it improves alignment of =/<-
+LPI_biome_summ <- LPI_long %>%
+  group_by(biome) %>%  # Group by biome
+  summarise(populations = n(),   # Create columns, number of populations
+            mean_study_length_years = mean(lengthyear),  # mean study length
+            max_lat = max(decimal_latitude),  # max latitude
+            min_lat = min(decimal_latitude),  # max longitude
+            dominant_sampling_method = names(which.max(table(sampling_method))),  # modal sampling method
+            dominant_units = names(which.max(table(units))))  # modal unit typ
+
+# no spaces around code in parentheses/brackets (unless it's a comma)
+# Good
+if (debug) do(x)
+diamonds[5, ]
+# Bad
+if ( debug ) do(x)  # No spaces around debug
+x[1,]   # Needs a space after the comma
+x[1 ,]  # Space goes after comma not before
+
+# inline commenting -- place tw spaces after code followed by # and single space then text
+# Calculating summary statistics for each biome in the Living Planet Index database
+# No need to copy and run this code now, this just illustrates comments
+LPI_biome_summ <- LPI2 %>%
+  group_by(biome) %>%  # Group by biome
+  summarise(populations = n(),   # Create columns, number of populations
+            mean_study_length_years = mean(lengthyear),  # mean study length
+            max_lat = max(decimal_latitude),  # max latitude
+            min_lat = min(decimal_latitude),  # max longitude
+            dominant_sampling_method = names(which.max(table(sampling_method))),  # modal sampling method
+            dominant_units = names(which.max(table(units))))  # modal unit type
+
+
+# Curly braces ----
+
+# opening curly brace should never go on its own line
+# should always be followed by a new line
+# closing curly brace should always go on its own line, unless it’s followed by else
+# always indent the code inside curly braces
+
+# Good
+
+if (y < 0 && debug) {
+  message("Y is negative")
+}
+
+if (y == 0) {
+  log(x)
+} else {
+  y ^ x
+}
+
+# Bad
+
+if (y < 0 && debug)
+{message("Y is negative")}
+
+if (y == 0) {
+  log(x)
+}
+else {
+  y ^ x
+}
+
+# Line length ----
+
+# convention = 80 characters per line 
+# Tools/Global Options/Code/Display/Show Margin/80 characters
+# when using pipes from dplyr package, keep pipe operator %>% at end of line
+
+LPI_long <- LPI_long %>%
+  group_by(., genus_species_id) %>%  # group rows so that each group is one population
+  mutate(., maxyear = max(year), minyear = min(year)) %>%  # Create columns for the first and most recent years that data was collected
+  mutate(., lengthyear = maxyear-minyear) %>%  # Create a column for the length of time data available
+  mutate(., scalepop = (pop-min(pop))/(max(pop)-min(pop))) %>%  # Scale population trend data
+  filter(., is.finite(scalepop)) %>%
+  filter(., lengthyear > 5) %>%  # Only keep rows with more than 5 years of data
+  ungroup(.)  # Remove any groupings you've greated in the pipe, not entirely necessary but it's better to be safe
+
+# when using ggplot2, keep + at end of line 
+(vulture_scatter <- ggplot(vultureITCR, aes (x = year, y = abundance, colour = Country.list)) +
+    geom_point(size = 2) +                                                
+    geom_smooth(method = lm, aes(fill = Country.list)) +                    
+    theme_my_own() +
+    scale_fill_manual(values = c("#EE7600", "#00868B")) +               
+    scale_colour_manual(values = c("#EE7600", "#00868B"),               
+                        labels = c("Croatia", "Italy")) +                 
+    ylab("Griffon vulture abundance\n") +                             
+    xlab("\nYear"))
+
 
